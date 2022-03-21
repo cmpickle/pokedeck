613df792fc44cb6ac09fe4f387c72208c13db265
Cameron Pickle <cmpickle@gmail.com>
2022-03-20 20:20:39 -0600

    Restarted with new deckshop django app

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Card
+
 def index(request):
-    return HttpResponse("This will be a pokemon card page")
+    cards = Card.objects.order_by('name')
+    template = loader.get_template('cards/index.html')
+    context = {
+        'cards': cards,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, id):
+    card = Card.objects.filter(number = id)[0]
+    template = loader.get_template('cards/detail.html')
+    context = {
+        'card': card,
+    }
+    return HttpResponse(template.render(context, request))
--- deckshop/asgi.py
+++ deckshop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pokedeck project.
+ASGI config for deckshop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokedeck.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deckshop.settings')
 
 application = get_asgi_application()
